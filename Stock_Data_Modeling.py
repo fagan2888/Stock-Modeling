bfe4773078e63c6dffa6fe97667af9f926709b6e
Matt Wilchek <mwilchek@gmu.edu>
2018-11-04 15:05:54 -0500

    Initial Commit

---
+++ Stock_Data_Modeling.py
@@ -0,0 +1,367 @@
+from __future__ import print_function
+from subprocess import call
+from pathlib import Path
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import RidgeCV
+import os
+import csv
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# Set directory to where data is
+# os.chdir(r'E:\Programming\Data Mining\GitHub Data Mining Project\GWU-Data-Mining-Proposal-1')
+
+# Execute script to get new data for today (after reddit web scraping)
+#scall('python process_reddit.py')
+import pandas as pd
+
+# Set Console formatting for panda prints
+pd.set_option('display.height', 1000)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.width', 1000)
+pd.options.mode.chained_assignment = None
+
+# **********************************************************************************************************************
+# Modeling / Prepare Data
+
+data = pd.read_csv('DJ_NEWS_SENTIMENT_DATA.csv')
+data_tomorrow = pd.read_csv('DJ_NEWS_SENTIMENT_DATA.csv')
+
+# Move certain columns up by one row for data_tomorrow
+data_tomorrow.Anger = data_tomorrow.Anger.shift(+1)
+data_tomorrow.Anticipation = data_tomorrow.Anticipation.shift(+1)
+data_tomorrow.Disgust = data_tomorrow.Disgust.shift(+1)
+data_tomorrow.Fear = data_tomorrow.Fear.shift(+1)
+data_tomorrow.Joy = data_tomorrow.Joy.shift(+1)
+data_tomorrow.Sadness = data_tomorrow.Sadness.shift(+1)
+data_tomorrow.Surprise = data_tomorrow.Surprise.shift(+1)
+data_tomorrow.Trust = data_tomorrow.Trust.shift(+1)
+data_tomorrow.Negative = data_tomorrow.Negative.shift(+1)
+data_tomorrow.Positive = data_tomorrow.Positive.shift(+1)
+data_tomorrow.Max_Sentiment = data_tomorrow.Max_Sentiment.shift(+1)
+data_tomorrow.Sentiment_Proportion = data_tomorrow.Sentiment_Proportion.shift(+1)
+
+# Delete the first row of data_tomorrow
+data_tomorrow.drop(data_tomorrow.head(1).index, inplace=True)
+
+train_data = data[:-1]  # train data
+today_record = data.tail(1)  # test data (validate current day and predict from following day)
+train_data_tomorrow = data_tomorrow[:-1]  # train data
+tomorrow_record = data_tomorrow.tail(1)  # test data (validate current day and predict from following day)
+
+
+########################################################################################################################
+# TODAY: Local method to identify most significant feature in dataset compared to y
+def identify_sig_feature_4_today(y_variable, graph_data):
+    # Split Data Into X, which are ALL the features
+    x = data.iloc[:, 9:18].values
+
+    # Split Data Into y, which are the associated targets/classifications; looking at Volume
+    y = data[np.unicode(y_variable)].values
+
+    # Get the Column Names, Ignore Index
+    feat_labels = data.columns[9:19]
+
+    # Start The Random Forest Classifier
+    treereg = RandomForestRegressor(max_depth=11, random_state=1)
+
+    # Execute The Data With The Random Forest Classifier
+    treereg.fit(x, y)
+
+    # Get The Important Features From The Classifier
+    importances = treereg.feature_importances_
+
+    # Sort The Features By The Most Important
+    indices = np.argsort(importances)[::-1]
+
+    # Return data
+    df_cols = ['Sentiment', 'Importance']
+    master_df = pd.DataFrame(columns=df_cols)
+
+    for f in range(x.shape[1]):
+        sentiment = feat_labels[f]
+        importance = importances[indices[f]]
+        temp_data = {'Sentiment': sentiment,
+                     'Importance': importance}
+        temp_df = pd.DataFrame(temp_data, columns=df_cols, index=[0])
+        master_df = master_df.append(temp_data, ignore_index=True)
+
+    highest_sentiment = master_df['Sentiment'].iloc[0]
+    highest_importance = master_df['Importance'].iloc[0]
+
+    if graph_data == "TRUE":
+        # Output Data As A Plot for Overall Data set
+        plt.title('Today Feature Importances ' + np.unicode(y_variable))
+        plt.bar(range(x.shape[1]), importances[indices], color='lightblue', align='center')
+        plt.xticks(range(x.shape[1]), feat_labels, rotation=90)
+        plt.xlim([-1, x.shape[1]])
+        plt.tight_layout()
+        plt.show()
+
+    return highest_sentiment, highest_importance
+
+
+########################################################################################################################
+# TOMORROW: Local method to identify most significant feature in dataset compared to y
+def identify_sig_feature_4_tomorrow(y_variable, graph_data):
+    # Split Data Into X, which are ALL the features
+    x = data_tomorrow.iloc[:, 9:18].values
+
+    # Split Data Into y, which are the associated targets/classifications; looking at Volume
+    y = data_tomorrow[np.unicode(y_variable)].values
+
+    # Get the Column Names, Ignore Index
+    feat_labels = data_tomorrow.columns[9:19]
+
+    # Start The Random Forest Classifier
+    treereg = RandomForestRegressor(max_depth=11, random_state=1)
+
+    # Execute The Data With The Random Forest Classifier
+    treereg.fit(x, y)
+
+    # Get The Important Features From The Classifier
+    importances = treereg.feature_importances_
+
+    # Sort The Features By The Most Important
+    indices = np.argsort(importances)[::-1]
+
+    # Return data
+    df_cols = ['Sentiment', 'Importance']
+    master_df = pd.DataFrame(columns=df_cols)
+
+    for f in range(x.shape[1]):
+        sentiment = feat_labels[f]
+        importance = importances[indices[f]]
+        temp_data = {'Sentiment': sentiment,
+                     'Importance': importance}
+        temp_df = pd.DataFrame(temp_data, columns=df_cols, index=[0])
+        master_df = master_df.append(temp_data, ignore_index=True)
+
+    highest_sentiment = master_df['Sentiment'].iloc[0]
+    highest_importance = master_df['Importance'].iloc[0]
+
+    if graph_data == "TRUE":
+        # Output Data As A Plot for Overall Data set
+        plt.title('Tomorrow Feature Importances ' + np.unicode(y_variable))
+        plt.bar(range(x.shape[1]), importances[indices], color='lightblue', align='center')
+        plt.xticks(range(x.shape[1]), feat_labels, rotation=90)
+        plt.xlim([-1, x.shape[1]])
+        plt.tight_layout()
+        plt.show()
+
+    return highest_sentiment, highest_importance
+
+
+########################################################################################################################
+# Local method to correctly retrieve appropriate paramters for Regularized Fit Regression based on Ridge Regression
+def get_fit_regression_params(significant_sentiment, sentiment_value):
+    # Define the data needed for this section, and as defined by highest_sentiment
+    x = data[significant_sentiment].values.reshape(-1, 1)
+    y = data.High
+
+    # Standardize features
+    scaler = StandardScaler()
+    x_std = scaler.fit_transform(x)
+    # Create ridge regression with alpha values from .1 to 10.0, in increments of 0.1
+    regr_cv = RidgeCV(alphas=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
+                              1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
+                              2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0,
+                              3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 3.0,
+                              4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0,
+                              5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0,
+                              6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0,
+                              7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0,
+                              8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9.0,
+                              9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0])
+    # Place x and y variables in the proper format for model_cv.
+    y = np.array(y)
+    x_std = x_std.reshape((len(y), 1))
+    y = y.reshape((len(y), 1))
+    # Determine the best alpha value to use.
+    model_cv = regr_cv.fit(x_std, y)
+    alpha_val_today = model_cv.alpha_
+
+    # Set the L1 value based on significant_sentiment_value
+    if sentiment_value >= 0.7:
+        weight_value = 0.4
+    elif sentiment_value >= 0.4:
+        weight_value = 0.5
+    else:
+        weight_value = 0.6
+
+    return alpha_val_today, weight_value
+
+
+########################################################################################################################
+# MODELING 4 TODAY #####################################################################################################
+# Prepare formula to predict closing of stock data for today
+
+highest_sentiment1_today, significant_value1_today = identify_sig_feature_4_today("Close", "False")
+formula = ('Close ~ Open + High + Low + ' + np.unicode(highest_sentiment1_today))
+dta = train_data[['Close', 'Open', 'High', 'Low', 'Anger', 'Anticipation',
+                  'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise',
+                  'Trust', 'Negative', 'Positive', 'Sentiment_Proportion']].copy()
+
+# set seed
+np.random.seed(1)
+
+alpha_val, weight_val = get_fit_regression_params(highest_sentiment1_today, significant_value1_today)
+
+# Create a Ordinary Least Squares regression model
+lm1_today = smf.ols(formula=formula, data=dta).fit_regularized(alpha=alpha_val, L1_wt=weight_val)
+fig1 = plt.figure(figsize=(12, 8))
+fig1 = sm.graphics.plot_partregress_grid(lm1_today, fig=fig1)
+fig1.savefig('Today_Close_Regression.png')  # Show Partial regression plot of model
+
+# Predicts closing value based on train data and model above
+today_close_prediction = lm1_today.predict(today_record)
+
+# Prepare formula to predict High of stock data for today
+highest_sentiment2_today, significant_value2_today = identify_sig_feature_4_today("High", "False")
+formula = ('High ~ Open + Close + Low + ' + np.unicode(highest_sentiment2_today))
+dta = train_data[['High', 'Open', 'Close', 'Low', 'Anger', 'Anticipation',
+                  'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise',
+                  'Trust', 'Negative', 'Positive', 'Sentiment_Proportion']].copy()
+
+alpha_val, weight_val = get_fit_regression_params(highest_sentiment2_today, significant_value2_today)
+
+# Create a Ordinary Least Squares regression model
+lm2_today = smf.ols(formula=formula, data=dta).fit_regularized(alpha=alpha_val, L1_wt=weight_val)
+fig2 = plt.figure(figsize=(12, 8))
+fig2 = sm.graphics.plot_partregress_grid(lm2_today, fig=fig2)
+fig2.savefig('Today_High_Regression.png')  # Show Partial regression plot of model
+
+# Predicts high value based on train data and model above
+today_high_prediction = lm2_today.predict(today_record)
+
+# Prepare formula to predict Low of stock data for today
+highest_sentiment3_today, significant_value3_today = identify_sig_feature_4_today("Low", "False")
+formula = ('Low ~ Open + Close + High + ' + np.unicode(highest_sentiment3_today))
+dta = train_data[['Low', 'Open', 'Close', 'High', 'Anger', 'Anticipation',
+                  'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise',
+                  'Trust', 'Negative', 'Positive', 'Sentiment_Proportion']].copy()
+
+alpha_val, weight_val = get_fit_regression_params(highest_sentiment3_today, significant_value3_today)
+
+# Create a Ordinary Least Squares regression model
+lm3_today = smf.ols(formula=formula, data=dta).fit_regularized(alpha=alpha_val, L1_wt=weight_val)
+fig3 = plt.figure(figsize=(12, 8))
+fig3 = sm.graphics.plot_partregress_grid(lm3_today, fig=fig3)
+fig3.savefig('Today_Low_Regression.png')  # Show Partial regression plot of model
+
+# Predicts Low value based on train data and model above
+today_low_prediction = lm3_today.predict(today_record)
+
+print("The Close value for today's stock is estimated to be: " + str(today_close_prediction.iloc[0]))
+print("The High value for today's stock is estimated to be: " + str(today_high_prediction.iloc[0]))
+print("The Low value for today's stock is estimated to be: " + str(today_low_prediction.iloc[0]))
+print("")
+
+########################################################################################################################
+# MODELING 4 NEXT DAY###################################################################################################
+
+highest_sentiment1_tom, significant_value1_tom = identify_sig_feature_4_tomorrow("Close", "False")
+formula = ('Close ~ Open + High + Low + ' + np.unicode(highest_sentiment1_tom))
+dta = train_data_tomorrow[['Close', 'Open', 'High', 'Low', 'Anger', 'Anticipation',
+                           'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise',
+                           'Trust', 'Negative', 'Positive', 'Sentiment_Proportion']].copy()
+# Set seed again
+np.random.seed(2)
+
+alpha_val, weight_val = get_fit_regression_params(highest_sentiment1_tom, significant_value1_tom)
+
+# Create a Ordinary Least Squares regression model
+lm1_tom = smf.ols(formula=formula, data=dta).fit_regularized(alpha=alpha_val, L1_wt=weight_val)
+fig4 = plt.figure(figsize=(12, 8))
+fig4 = sm.graphics.plot_partregress_grid(lm1_tom, fig=fig4)
+fig4.savefig('Tomorrow_Close_Regression.png')  # Show Partial regression plot of model
+
+# Predicts closing value based on train data and model above
+close_prediction_tom = lm1_tom.predict(tomorrow_record)
+
+# Prepare formula to predict High of stock data for today
+highest_sentiment2_tom, significant_value2_tom = identify_sig_feature_4_tomorrow("High", "False")
+formula = ('High ~ Open + Close + Low + ' + np.unicode(highest_sentiment2_tom))
+dta = train_data_tomorrow[['High', 'Open', 'Close', 'Low', 'Anger', 'Anticipation',
+                           'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise',
+                           'Trust', 'Negative', 'Positive', 'Sentiment_Proportion']].copy()
+
+alpha_val, weight_val = get_fit_regression_params(highest_sentiment2_tom, significant_value2_tom)
+
+# Create a Ordinary Least Squares regression model
+lm2_tom = smf.ols(formula=formula, data=dta).fit_regularized(alpha=alpha_val, L1_wt=weight_val)
+fig5 = plt.figure(figsize=(12, 8))
+fig5 = sm.graphics.plot_partregress_grid(lm2_tom, fig=fig5)
+fig5.savefig('Tomorrow_High_Regression.png')  # Show Partial regression plot of model
+
+# Predicts high value based on train data and model above
+high_prediction_tom = lm2_tom.predict(tomorrow_record)
+
+# Prepare formula to predict Low of stock data for today
+highest_sentiment3_tom, significant_value3_tom = identify_sig_feature_4_tomorrow("Low", "False")
+formula = ('Low ~ Open + Close + High + ' + np.unicode(highest_sentiment3_tom))
+dta = train_data_tomorrow[['Low', 'Open', 'Close', 'High', 'Anger', 'Anticipation',
+                           'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise',
+                           'Trust', 'Negative', 'Positive', 'Sentiment_Proportion']].copy()
+
+alpha_val, weight_val = get_fit_regression_params(highest_sentiment3_tom, significant_value3_tom)
+
+# Create a Ordinary Least Squares regression model
+lm3_tom = smf.ols(formula=formula, data=dta).fit_regularized(alpha=alpha_val, L1_wt=weight_val)
+fig6 = plt.figure(figsize=(12, 8))
+fig6 = sm.graphics.plot_partregress_grid(lm3_tom, fig=fig6)
+fig6.savefig('Tomorrow_Low_Regression.png')  # Show Partial regression plot of model
+
+# Predicts Low value based on train data and model above
+low_prediction_tom = lm3_tom.predict(tomorrow_record)
+
+print("The Close value for tomorrow's stock is estimated to be: " + str(close_prediction_tom.iloc[0]))
+print("The High value for tomorrow's stock is estimated to be: " + str(high_prediction_tom.iloc[0]))
+print("The Low value for tomorrow's stock is estimated to be: " + str(low_prediction_tom.iloc[0]))
+print("")
+
+if float(today_close_prediction.iloc[0]) < float(close_prediction_tom.iloc[0]):
+    print("Based on our algorithm, the Closing value for the stock tomorrow will: Increase")
+else:
+    print("Based on our algorithm, the Closing value for the stock tomorrow will: Decrease")
+
+if float(today_high_prediction.iloc[0]) < float(high_prediction_tom.iloc[0]):
+    print("Based on our algorithm, the High value for the stock tomorrow will: Increase")
+else:
+    print("Based on our algorithm, the High value for the stock tomorrow will: Decrease")
+
+if float(today_low_prediction.iloc[0]) < float(low_prediction_tom.iloc[0]):
+    print("Based on our algorithm, the Low value for the stock tomorrow will: Increase")
+else:
+    print("Based on our algorithm, the Low value for the stock tomorrow will: Decrease")
+
+# Record data today data and predictions to analyze accuracy:
+with open('predictions_djia.csv', 'a') as csvfile:
+    now = datetime.datetime.now()
+    date = now.strftime("%m/%d/%Y")
+    names = ['Date', 'Actual Close Today', 'Actual High Today', 'Actual Low Today', 'Predicted Close Today',
+             'Predicted High Today', 'Predicted Low Today', 'Predicted Close Tomorrow', 'Predicted High Tomorrow',
+             'Predicted Low Tomorrow']
+    w = csv.DictWriter(csvfile, fieldnames=names, lineterminator='\n')
+    w.writerow({'Date': str(date),
+                'Actual Close Today': today_record['Close'].iloc[0],
+                'Actual High Today': today_record['High'].iloc[0],
+                'Actual Low Today': today_record['Low'].iloc[0],
+                'Predicted Close Today': today_close_prediction.iloc[0],
+                'Predicted High Today': today_high_prediction.iloc[0],
+                'Predicted Low Today': today_low_prediction.iloc[0],
+                'Predicted Close Tomorrow': close_prediction_tom.iloc[0],
+                'Predicted High Tomorrow': high_prediction_tom.iloc[0],
+                'Predicted Low Tomorrow': low_prediction_tom.iloc[0]})
+
+    csvfile.close()
+
+# **********************************************************************************************************************
+########################################################################################################################
---
+++ process_reddit.py
@@ -0,0 +1,146 @@
+"""
+This script will process reddit news data downloaded from the Reddit Data Extractor into a csv file.
+Perform Sentiment Analysis on it using a separate R script
+Collect Stock data from yahoo finance
+Join the sentiment analysis and yahoo finance data together
+To modify data collection edit lines 33 and 41 with different ticker, line 55 with path to news data, 58 with path to 
+where this script is saved, and line 79, 80 to path where Rscript.exe and sentiment_extraction.R are saved
+This script can be run separately w/o Data_Mining_Analysis.py just for the data collection
+"""
+import os
+from subprocess import call
+from datetime import datetime
+import time
+import shutil
+
+
+########################################################################################################################
+# execute yahoo-finance to get the DJIA data first
+# Parameters that can be changed: Stock Ticker and Data Timeframe for Extraction
+# Currently set to DJIA and Data for Today
+def get_yahoo_data():
+    from pandas_datareader import data as pdr
+    from datetime import datetime, timedelta
+    import fix_yahoo_finance as yf
+    yf.pdr_override()
+
+    now = datetime.now()
+    end_day = now + timedelta(days=1)
+
+    # download dataframe from yahoo (^DJI or BTCUSD=X)
+    data = pdr.get_data_yahoo("^DJI", start=now.strftime("%Y-%m-%d"), end=end_day.strftime("%Y-%m-%d"))
+    try:
+        data['change'] = (data['Close'].values.astype(int) - data['Open'].values.astype(int)) / data['Open'].values.astype(int)
+        data['change'] = data['change'] * 100
+
+    except KeyError:
+        import fix_yahoo_finance as yf
+        yf.pdr_override()
+        data = pdr.get_data_yahoo("^DJI", start=end_day.strftime("%Y-%m-%d"), end=now.strftime("%Y-%m-%d"))
+        data['change'] = ((data['Close'].values).astype(int) - (data['Open'].values).astype(int)) / (
+            data['Open'].values).astype(int)
+        data['change'] = data['change'] * 100
+
+    return data
+
+
+new_djia_data = get_yahoo_data()
+new_djia_data = new_djia_data.tail(1)
+########################################################################################################################
+# re-import pandas to reset the override that is needed for yahoo-finance
+# set the path to where the reddit news data is
+# set the paths to Rscript.exe from your R build and path to the sentiment_extraction.r file
+import pandas as pd
+
+# set directory where scripts are
+os.chdir(r'E:\Programming\Data Mining\GitHub Data Mining Project\GWU-Data-Mining-Proposal-1')
+
+dir_name = os.getcwd() + r'\reddit_data\worldnews'  # change to where the reddit files are
+data_path = os.listdir(dir_name)
+date = time.strftime("%Y-%m-%d")
+df_cols = ['Date', 'News']
+master_df = pd.DataFrame(columns=df_cols)
+
+# This chunk process the reddit news data into one master csv file for sentiment analysis ##############################
+for item in data_path:
+    string = str(item)
+    if string[-4:] == ".txt" and len(string) > 10:
+        news = string.replace('.txt', '')
+        temp_data = {'Date': date,
+                     'News': news}
+        temp_df = pd.DataFrame(temp_data, columns=df_cols, index=[0])
+        master_df = master_df.append(temp_data, ignore_index=True)
+
+master_df.to_csv('Today_News.csv', encoding='utf-8', index=False, sep=',')
+# The above file will be made where this python script is
+# The absolute path to this file must be used in the sentiment_extraction.R file (please revise the R file)
+
+shutil.rmtree(dir_name)  # removes all the files collected from the reddit web scraper
+########################################################################################################################
+# run R script and return new_sentiment_data.csv
+# NOTE: param1 = path to Rscript.exe from the R build, param2 is the path to the R file to execute
+call(["C:/Users/Matt Wilchek/Documents/R/R-3.4.3/bin/Rscript",
+      "E:/Programming/Data Mining/GitHub Data Mining Project/GWU-Data-Mining-Proposal-1/sentiment_extraction.r"])
+# E:/Programming/Data Mining/GitHub Data Mining Project/GWU-Data-Mining-Proposal-1/sentiment_extraction.r
+sentiment = pd.read_csv("new_sentiment_data.csv")
+sentiment.drop('Unnamed: 0', axis=1, inplace=True)
+########################################################################################################################
+# Format DJIA dataframe to original dataset format
+new_djia_data = new_djia_data.reset_index(drop=True)
+now = datetime.now()
+new_djia_data['Date'] = now.strftime("%m/%d/%Y")  # 8/8/2008
+cols = new_djia_data.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+new_djia_data = new_djia_data[cols]
+
+# Append Sentiment Dataframe columns to DJIA Dataframe columns
+new_djia_data['Anger'] = sentiment['Anger']
+new_djia_data['Anticipation'] = sentiment['Anticipation']
+new_djia_data['Disgust'] = sentiment['Disgust']
+new_djia_data['Fear'] = sentiment['Fear']
+new_djia_data['Joy'] = sentiment['Joy']
+new_djia_data['Sadness'] = sentiment['Sadness']
+new_djia_data['Surprise'] = sentiment['Surprise']
+new_djia_data['Trust'] = sentiment['Trust']
+new_djia_data['Negative'] = sentiment['Negative']
+new_djia_data['Positive'] = sentiment['Positive']
+
+# Calculate Sentiment Proportions of Original Data
+feat_labels = new_djia_data.columns[8:18]
+sentiment_data = new_djia_data.iloc[:, 8:18].values
+temp_df = pd.DataFrame(sentiment_data, columns=feat_labels, index=None)
+temp_list = list()
+for l, n in temp_df.iterrows():
+    max_sentiment = n.idxmax()
+    max_sentiment_value = ((n.max(axis=0)).astype(int)).max()
+    total_values = sum(n)
+    temp_list.append([str(max_sentiment), max_sentiment_value / total_values])
+
+temp_df2 = pd.DataFrame(temp_list, columns=['Max_Sentiment', 'Sentiment_Proportion'], index=None)
+new_djia_data = pd.concat([new_djia_data, temp_df2], axis=1)
+new_djia_data.columns.values[5] = "Adj.Close"
+new_djia_data = new_djia_data.reset_index(drop=True)
+new_order = [0, 1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+new_djia_data = new_djia_data[new_djia_data.columns[new_order]]
+
+new_djia_data.to_csv('new_record.csv', encoding='utf-8', index=False, sep=',')
+os.remove('new_sentiment_data.csv')  # removes the combined DJIA and sentiment data file
+
+########################################################################################################################
+# Add New Data if it exists to master dataset
+new_record = os.path.exists('new_record.csv')
+
+if new_record:
+    df_sentiment_dj = pd.read_csv('DJ_NEWS_SENTIMENT_DATA.csv')
+    new_record_row = pd.read_csv('new_record.csv')
+    df_sentiment_dj = df_sentiment_dj.append(new_record_row, ignore_index=True)
+    df_sentiment_dj = df_sentiment_dj[['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj.Close', 'change', 'Anger',
+                                       'Anticipation', 'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise', 'Trust',
+                                       'Negative', 'Positive', 'Max_Sentiment', 'Sentiment_Proportion']]
+    os.remove('new_record.csv')
+
+    # Update old dataset
+    os.remove('DJ_NEWS_SENTIMENT_DATA.csv')
+    df_sentiment_dj.to_csv('DJ_NEWS_SENTIMENT_DATA.csv', encoding='utf-8', index=False, sep=',')
+
+__author__ = 'Matt Wilchek'
