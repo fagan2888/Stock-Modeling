eeb6c211a2dc48c8432ab07bf7b11a93d09827f0
eric goldman <eric.goldman16@gmail.com>
2018-11-18 14:47:01 -0500

    new file

--- Stock_Data_Modeling.py
+++ Stock_Data_Modeling.py
@@ -36,7 +36,8 @@ pd.options.mode.chained_assignment = None
 
 # **********************************************************************************************************************
 # Modeling / Prepare Data
-# data = pd.read_csv('DJ_NEWS_SENTIMENT_DATA.csv')
+data = pd.read_csv('DJ_NEWS_SENTIMENT_DATA.csv')
+data['Cycle_Change'] = data.Max_Sentiment.eq(data.Max_Sentiment.shift())
 # data_tomorrow = pd.read_csv('DJ_NEWS_SENTIMENT_DATA.csv')
 
 data = pd.read_csv('https://raw.githubusercontent.com/mwilchek/Stock-Modeling/master/DJ_NEWS_SENTIMENT_DATA%20eg.csv')
